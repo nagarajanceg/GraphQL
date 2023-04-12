ff2d222f1d83f4ac7cc646d7438d7a187b7b4803
nagarajan.ceg <nagharajang@gmail.com>
2023-04-12 11:22:15 -0700

    Test out Queries and Mutations

--- index.js
+++ index.js
@@ -1,39 +1,26 @@
-import express  from "express";
+import express from "express";
 import { graphqlHTTP } from "express-graphql";
-import schema from './schema';
+import schema from "./schema";
+import resolvers from "./resolver";
 
-const app = express()
+const app = express();
 
-app.get("/",(req,res)=>{
-    res.send("GraphQL is Amazing!")
+
+app.get("/", (req, res) => {
+    res.send("GraphQL is Amazing!");
 });
 
-const root = {
-    product : () =>{
-        
-        return{
-            "id":"123443",
-            "name": "widget",
-            "description": "Beautiful widget to use in your garden",
-            "price": 23.99,
-            "soldout": false,
-            "stores": [
-                {
-                    store:"San Jose"
-                },
-                {
-                    store: "SF"
-                }
-            ]
-        }
-    }
-};
 
-app.use('/graphql', graphqlHTTP({
-    schema,
-    rootValue:root,
-    graphiql: true
-}))
-app.listen(8080,()=>{
+const root = resolvers;
+
+app.use(
+    "/graphql",
+    graphqlHTTP({
+        schema,
+        rootValue: root,
+        graphiql: true,
+    })
+);
+app.listen(8080, () => {
     console.log("running server on port localhost:8080/graphql");
-})
+});
--- schema.js
+++ schema.js
@@ -1,21 +1,45 @@
 import { buildSchema } from "graphql";
 
 const schema = buildSchema(`
-type Product{
-    id:ID,
-    name: String,
-    description: String,
-    price: Float,
-    soldout: Boolean
-    stores: [Store]!
-}
-
-type Store{
-    store: String
-}
-    type Query{
-        product: Product
+    type Product{
+        id:ID,
+        name: String,
+        description: String,
+        price: Float,
+        quantity: Int
+        soldout: Soldout
+        stores: [Store]!
     }
-`)
+
+    enum Soldout{
+        SOLDOUT
+        ONSALE
+    }
+    type Store{
+        store: String
+    }
+
+    type Query{
+        getProduct(id: ID): Product
+    }
+
+    input StoreInput{
+        store: String
+    }
+
+    input ProductInput{
+        id: ID,
+        name: String,
+        description: String,
+        price: Float,
+        quantity: Int
+        soldout: Soldout
+        stores: [StoreInput]!
+    }
+
+    type Mutation{
+        createProduct(input: ProductInput): Product
+    }
+`);
 
 export default schema;
